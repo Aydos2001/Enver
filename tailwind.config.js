d052bc10ef18e5453eb0ea2d2ed37c4eca3f0d93
Aydos2001 <nawrizbaevaydos15@gmail.com>
2024-04-26 19:38:54 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+  theme: {
+      extend: {
+          screens: {
+              "sm": "425px",
+              "md": "768px",
+              "lg": "1024px",
+              "xl": "1200px"
+          },
+          colors: {
+              primary: "#5454D4",
+              primaryBg: "#19191B",
+          },
+          fontFamily: {
+              poppins : '"Poppins", sans-serif'
+          },
+      }
+  }
+}
